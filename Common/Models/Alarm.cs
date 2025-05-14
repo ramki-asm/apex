e1caa69353a1acf95c7c1406b452e4207e1ea6f6
A8753 <A8753@INBGNL0642>
2025-05-14 22:23:12 +0530

    Create Branch for mastsr

---
+++ Common/Models/Alarm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public class Alarm
+    {
+        public int Serial { get; set; }
+        public string Module { get; set; }
+        public string Content { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ UI/UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,97 @@
+﻿using APEX.Common.Helpers;
+using FontAwesome.Sharp;
+using System.Windows.Input;
+using UI.ViewModels;
+
+namespace UI.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase _mainFormViewModel;
+        private string _caption;
+        private IconChar _icon;
+
+        public ViewModelBase MainFormViewModel
+        {
+            get => _mainFormViewModel;
+            set
+            {
+                _mainFormViewModel = value;
+                OnPropertyChanged(nameof(MainFormViewModel));
+            }
+        }
+
+        public string Caption
+        {
+            get => _caption;
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+
+        public IconChar Icon
+        {
+            get => _icon;
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+         
+        public ICommand ShowHomeViewCommand { get; }
+        public ICommand ShowCustomerViewCommand { get; }
+        public ICommand ShowSettingViewCommand { get; }
+        public ICommand ShowAlarmViewCommand { get; }
+public ICommand ShowDashboardCommand { get; }
+
+      public  MainViewModel()
+        {
+            // Initialize commands
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
+            ShowSettingViewCommand = new ViewModelCommand(ExecuteShowSettingViewCommand);
+            ShowAlarmViewCommand = new ViewModelCommand(ExecuteShowAlarmViewCommand);
+            ShowDashboardCommand = new ViewModelCommand(ExecuteShowDashboardCommand);
+            ExecuteShowHomeViewCommand(null);
+        }
+
+        private void ExecuteShowAlarmViewCommand(object obj)
+        {
+            MainFormViewModel = new AlarmViewModel();
+            Caption = "Alarms";
+            Icon = IconChar.Home;
+        }
+
+        private void ExecuteShowDashboardCommand(object obj)
+        {
+            MainFormViewModel = new DashboardViewModel();
+            Caption = "Dashboard";
+            Icon = IconChar.Home;
+        }
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            MainFormViewModel = new HomeViewModel();
+            Caption = "Home";
+            Icon = IconChar.Home;
+        }
+
+        private void ExecuteShowCustomerViewCommand(object obj)
+        {
+            MainFormViewModel = new CustomerViewModel();
+            Caption = "Customers";
+            Icon = IconChar.UserGroup;
+        }
+
+        private void ExecuteShowSettingViewCommand(object obj)
+        {
+            MainFormViewModel = new SettingViewModel();
+            Caption = "Settings";
+            Icon = IconChar.Gear;
+        }
+    }
+}
