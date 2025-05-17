f0f51e4a68404200a3fd206b0d77894d9d011142
A8753 <A8753@INBGNL0642>
2025-05-17 10:16:58 +0530

    Adding logs

--- UI/ViewModels/MainViewModel.cs
+++ UI/ViewModels/MainViewModel.cs
@@ -1,123 +1,26 @@
-﻿//using APEX.Common.Helpers;
-//using FontAwesome.Sharp;
-//using System.Windows.Input;
-//using UI.ViewModels;
-
-//namespace UI.ViewModels
-//{
-//    public class MainViewModel : ViewModelBase
-//    {
-//        private ViewModelBase _mainFormViewModel;
-//        private string _caption;
-//        private IconChar _icon;
-
-//        public ViewModelBase MainFormViewModel
-//        {
-//            get => _mainFormViewModel;
-//            set
-//            {
-//                _mainFormViewModel = value;
-//                OnPropertyChanged(nameof(MainFormViewModel));
-//            }
-//        }
-
-//        public string Caption
-//        {
-//            get => _caption;
-//            set
-//            {
-//                _caption = value;
-//                OnPropertyChanged(nameof(Caption));
-//            }
-//        }
-
-//        public IconChar Icon
-//        {
-//            get => _icon;
-//            set
-//            {
-//                _icon = value;
-//                OnPropertyChanged(nameof(Icon));
-//            }
-//        }
-
-
-//        public ICommand ShowHomeViewCommand { get; }
-//        public ICommand ShowCustomerViewCommand { get; }
-//        public ICommand ShowSettingViewCommand { get; }
-//        public ICommand ShowAlarmViewCommand { get; }
-//public ICommand ShowDashboardCommand { get; }
-
-//      public  MainViewModel()
-//        {
-//            // Initialize commands
-//            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
-//            ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
-//            ShowSettingViewCommand = new ViewModelCommand(ExecuteShowSettingViewCommand);
-//            ShowAlarmViewCommand = new ViewModelCommand(ExecuteShowAlarmViewCommand);
-//            ShowDashboardCommand = new ViewModelCommand(ExecuteShowDashboardCommand);
-//            ExecuteShowHomeViewCommand(null);
-//        }
-
-//        private void ExecuteShowAlarmViewCommand(object obj)
-//        {
-//            MainFormViewModel = new AlarmViewModel();
-//            Caption = "Alarms";
-//            Icon = IconChar.Home;
-//        }
-
-//        private void ExecuteShowDashboardCommand(object obj)
-//        {
-//            MainFormViewModel = new DashboardViewModel();
-//            Caption = "Dashboard";
-//            Icon = IconChar.Home;
-//        }
-
-//        private void ExecuteShowHomeViewCommand(object obj)
-//        {
-//            MainFormViewModel = new HomeViewModel();
-//            Caption = "Home";
-//            Icon = IconChar.Home;
-//        }
-
-//        private void ExecuteShowCustomerViewCommand(object obj)
-//        {
-//            MainFormViewModel = new CustomerViewModel();
-//            Caption = "Customers";
-//            Icon = IconChar.UserGroup;
-//        }
-
-//        private void ExecuteShowSettingViewCommand(object obj)
-//        {
-//            MainFormViewModel = new SettingViewModel();
-//            Caption = "Settings";
-//            Icon = IconChar.Gear;
-//        }
-//    }
-//}
- 
-using Common.Models;
+﻿using Common.Models;
+using log4net;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Windows.Input;
+using UI.ViewModels;
 
 namespace UI.ViewModels
 {
     public class MainViewModel : BindableBase
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(MainViewModel));
         private BindableBase _mainFormViewModel;
         private string _caption;
-        //private IconChar _icon;
         private ObservableCollection<MenuButtonConfig> _menuButtons;
 
         public BindableBase MainFormViewModel
         {
             get => _mainFormViewModel;
-            set=> SetProperty(ref _mainFormViewModel, value);
-            
+            set => SetProperty(ref _mainFormViewModel, value);
         }
 
         public string Caption
@@ -126,52 +29,41 @@ namespace UI.ViewModels
             set => SetProperty(ref _caption, value);
         }
 
-        //public IconChar Icon
-        //{
-        //    get => _icon;
-        //    set
-        //    {
-        //        _icon = value;
-        //        OnPropertyChanged(nameof(Icon));
-        //    }
-        //}
-
         public ObservableCollection<MenuButtonConfig> MenuButtons
         {
             get => _menuButtons;
             set => SetProperty(ref _menuButtons, value);
         }
 
-        public DelegateCommand<Object> ShowHomeViewCommand { get; }
-        public DelegateCommand<Object> ShowCustomerViewCommand { get; }
-        public DelegateCommand<Object> ShowSettingViewCommand { get; }
-        public DelegateCommand<Object> ShowAlarmViewCommand { get; }
-        public DelegateCommand<Object> ShowDashboardCommand { get; }
+        public DelegateCommand<object> ShowHomeViewCommand { get; }
+        public DelegateCommand<object> ShowCustomerViewCommand { get; }
+        public DelegateCommand<object> ShowSettingViewCommand { get; }
+        public DelegateCommand<object> ShowAlarmViewCommand { get; }
+        public DelegateCommand<object> ShowDashboardCommand { get; }
+        public DelegateCommand<object> ShowUserViewCommand { get; }
 
-        public DelegateCommand<Object> ShowUserViewCommand { get; }
         public MainViewModel()
         {
-            // Initialize commands
-            ShowHomeViewCommand = new DelegateCommand<Object>(ExecuteShowHomeViewCommand);
-            ShowCustomerViewCommand = new DelegateCommand<Object>(ExecuteShowCustomerViewCommand);
-            ShowSettingViewCommand = new DelegateCommand<Object>(ExecuteShowSettingViewCommand);
-            ShowAlarmViewCommand = new DelegateCommand<Object>(ExecuteShowAlarmViewCommand);
-            ShowDashboardCommand = new DelegateCommand<Object>(ExecuteShowDashboardCommand);
-            ShowUserViewCommand = new DelegateCommand<Object>(ExecuteShowSettingViewCommand);
-            LoadMenuButtons();
+            Log.Info("MainViewModel constructor called");
+            ShowHomeViewCommand = new DelegateCommand<object>(ExecuteShowHomeViewCommand);
+            ShowCustomerViewCommand = new DelegateCommand<object>(ExecuteShowCustomerViewCommand);
+            ShowSettingViewCommand = new DelegateCommand<object>(ExecuteShowSettingViewCommand);
+            ShowAlarmViewCommand = new DelegateCommand<object>(ExecuteShowAlarmViewCommand);
+            ShowDashboardCommand = new DelegateCommand<object>(ExecuteShowDashboardCommand);
+            ShowUserViewCommand = new DelegateCommand<object>(ExecuteShowUserViewCommand);
 
-            //ExecuteShowHomeViewCommand(null);
+            LoadMenuButtons();
+            ExecuteShowHomeViewCommand(null); // Set default view
         }
 
         private void LoadMenuButtons()
         {
             try
             {
-                // Read the JSON file
                 string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "MenuButtons.json");
                 if (!File.Exists(jsonPath))
                 {
-                    // If the file doesn't exist, initialize an empty collection
+                    Log.Warn($"MenuButtons.json not found at {jsonPath}");
                     MenuButtons = new ObservableCollection<MenuButtonConfig>();
                     return;
                 }
@@ -179,89 +71,77 @@ namespace UI.ViewModels
                 string json = File.ReadAllText(jsonPath);
                 var allButtons = JsonConvert.DeserializeObject<List<MenuButtonConfig>>(json);
 
-                // Map command Language to actual ICommand properties
                 var commandMap = new Dictionary<string, ICommand>
                 {
                     { "ShowDashboardCommand", ShowDashboardCommand },
                     { "ShowHomeViewCommand", ShowHomeViewCommand },
                     { "ShowCustomerViewCommand", ShowCustomerViewCommand },
                     { "ShowSettingViewCommand", ShowSettingViewCommand },
-                    { "ShowAlarmViewCommand", ShowAlarmViewCommand }
+                    { "ShowAlarmViewCommand", ShowAlarmViewCommand },
+                    { "ShowUserViewCommand", ShowUserViewCommand }
                 };
 
                 foreach (var button in allButtons)
                 {
-                    // Map the Icon string to IconChar
-                    //if (Enum.TryParse<IconChar>(button.Icon, out var iconChar))
-                    //{
-                    //    button.Icon = iconChar.ToString(); // Ensure consistency
-                    //}
-
-                     
                     if (commandMap.TryGetValue(button.CommandName, out var command))
                     {
                         button.Command = command;
                     }
                     else
                     {
-                        
-                        Console.WriteLine($"Command '{button.CommandName}' not found for button '{button.Text}'.");
+                        Log.Warn($"Command '{button.CommandName}' not found for button '{button.Text}'.");
                     }
                 }
 
-                // Filter buttons based on configuration
-                MenuButtons = new ObservableCollection<MenuButtonConfig>(allButtons.Where(button =>
-                      ShouldShowButton(button)));
+                MenuButtons = new ObservableCollection<MenuButtonConfig>(allButtons.Where(ShouldShowButton));
             }
             catch (Exception ex)
             {
-                // Handle errors (e.g., log the error, show a message)
-                Console.WriteLine($"Error loading menu buttons: {ex.Message}");
+                Log.Error("Error loading menu buttons", ex);
                 MenuButtons = new ObservableCollection<MenuButtonConfig>();
             }
         }
 
         private bool ShouldShowButton(MenuButtonConfig button)
         {
-
-            return ( button.IsVisible);
- 
+            Log.Info($"Checking visibility for button: {button.Text}");
+            return button.IsVisible;
         }
 
         private void ExecuteShowAlarmViewCommand(object obj)
         {
             MainFormViewModel = new AlarmViewModel();
             Caption = "Alarms";
-            //Icon = IconChar.Home;
         }
 
         private void ExecuteShowDashboardCommand(object obj)
         {
             MainFormViewModel = new DashboardViewModel();
             Caption = "Dashboard";
-           // Icon = IconChar.Home;
         }
 
         private void ExecuteShowHomeViewCommand(object obj)
         {
-            
             MainFormViewModel = new HomeViewModel();
             Caption = "Home";
-            //Icon = IconChar.Home;
         }
 
         private void ExecuteShowCustomerViewCommand(object obj)
         {
             MainFormViewModel = new CustomerViewModel();
             Caption = "Customers";
-         //  Icon = IconChar.UserGroup;
         }
 
         private void ExecuteShowSettingViewCommand(object obj)
         {
             MainFormViewModel = new SettingViewModel();
             Caption = "Settings";
-           // Icon = IconChar.Gear;
+        }
+
+        private void ExecuteShowUserViewCommand(object obj)
+        {
+            MainFormViewModel = new UserViewModel();
+            Caption = "Users";
         }
     }
 }
